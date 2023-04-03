18fedd9312b223c97609f118cb08d9c4ea93d736
Julien Brayere <julien.brayere@obitrain.com>
2023-04-03 15:16:41 +0200

    feat: adding ruff check python tests

---
+++ src/scripts/python/python-lint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Ruff
+poetry run ruff .
--- src/scripts/python/python-tests.sh
+++ src/scripts/python/python-tests.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Pyright
-poetry run pyright
-
 # Pytest
 poetry run pytest -vv --junitxml=tests/junit.xml
-
 # Coverage
 poetry run coverage html
