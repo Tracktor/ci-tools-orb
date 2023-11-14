d908f4fbb1b5f349d979ecdd25dd81a9610b305a
Julien Brayere <julien.brayere@obitrain.com>
2023-11-14 15:04:38 +0100

    test: update ci-user

--- src/scripts/bash/bump-version.sh
+++ src/scripts/bash/bump-version.sh
@@ -3,6 +3,9 @@
 # Utility script to bump the version and push to git
 # Usage: ./bump-version.sh <python|js> <branch>
 # If no branch is provided, it will default to master
+# For this script to work, you need to have the following environment variables set:
+# - CI_EMAIL
+# - CI_USER
 # You can also set the following environment variables:
 # - DRY_RUN: if set to true, it will not push to git
 # You will need to have the following tools installed:
@@ -93,6 +96,23 @@ function bump_push_js() {
     echo "Dry run, not pushing"
   fi
 }
+
+function git_init() {
+  if [ -z "$CI_EMAIL" ]; then
+    echo "CI_EMAIL is not set"
+    exit 1
+  fi
+  if [ -z "$CI_USER" ]; then
+    echo "CI_USER is not set"
+    exit 1
+  fi
+  echo "Setting user.email to $CI_EMAIL"
+  git config --global user.email "$CI_EMAIL"
+  echo "Setting user.name to $CI_USER"
+  git config --global user.name "$CI_USER"
+}
+
+git_init
 
 case $LANG_TYPE in
 python)
