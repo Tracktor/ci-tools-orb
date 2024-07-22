54526ad2b05d32fb29b32ecfe0560d80f3e44123
Julien Brayere <julien.brayere@obitrain.com>
2024-07-22 15:10:39 +0200

    chore: fix track-bump

--- src/scripts/bash/track-bump.sh
+++ src/scripts/bash/track-bump.sh
@@ -8,13 +8,19 @@ if [ "$DRY_RUN" -eq "1" ]; then
   _DRY_RUN="true"
 fi
 
+_VERBOSE=""
+# shellcheck disable=SC2153
+if [ "$VERBOSE" -eq "1" ]; then
+  _VERBOSE="-vv"
+fi
+
 _flags=""
 # shellcheck disable=SC2153
 if [ "$SIGN_COMMIT" -eq "1" ]; then
   _flags="--sign"
 fi
 
-pipx run track-bump bump --branch "$BRANCH" "$_flags" "$CUSTOM_PARAMS"
+pipx run track-bump "$_VERBOSE" bump --branch "$BRANCH" "$_flags" "$CUSTOM_PARAMS"
 
 if [ "$_DRY_RUN" = "false" ]; then
   git push origin "$BRANCH" --follow-tags
