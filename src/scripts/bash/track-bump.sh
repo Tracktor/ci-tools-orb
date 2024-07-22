c5d5a5fdf8f5b07e42da7f1318df639dae27ea72
Julien Brayere <julien.brayere@obitrain.com>
2024-07-22 15:13:53 +0200

    chore: fix track-bump

--- src/scripts/bash/track-bump.sh
+++ src/scripts/bash/track-bump.sh
@@ -20,10 +20,16 @@ if [ "$SIGN_COMMIT" -eq "1" ]; then
   _flags="--sign"
 fi
 
-pipx run track-bump "$_VERBOSE" bump --branch "$BRANCH" "$_flags" "$CUSTOM_PARAMS"
+_BRANCH=""
+# shellcheck disable=SC2153
+if [ -z "$BRANCH" ]; then
+  _BRANCH=$(git branch --show-current)
+fi
+
+pipx run track-bump "$_VERBOSE" bump --branch "$_BRANCH" "$_flags" "$CUSTOM_PARAMS"
 
 if [ "$_DRY_RUN" = "false" ]; then
-  git push origin "$BRANCH" --follow-tags
+  git push origin "$_BRANCH" --follow-tags
   gh release create -F CHANGELOG.md "$TAG"
 else
   echo "Dry run, not pushing (branch: $BRANCH, version: $TAG)"
