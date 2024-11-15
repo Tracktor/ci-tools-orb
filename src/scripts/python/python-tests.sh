adda7e4596fccc19952dc177b04093004f70c71e
Julien Brayere <julien.brayere@obitrain.com>
2024-11-15 18:08:28 +0100

    fix: adding tests-extras params

--- src/scripts/python/python-tests.sh
+++ src/scripts/python/python-tests.sh
@@ -3,6 +3,9 @@
 
 
 # Pytest
-poetry run pytest -vv --junitxml=tests/junit.xml "$@"
+if [[ "${EXTRA_PARAMS}" == "" ]]; then
+  poetry run pytest -vv --junitxml=tests/junit.xml ${EXTRA_PARAMS}
+else
+  poetry run pytest -vv --junitxml=tests/junit.xml
 # Coverage
 poetry run coverage html
