27f7c3658d92100bb2be7b44e874dcd840839928
Julien Brayere <julien.brayere@obitrain.com>
2024-11-15 18:21:02 +0100

    fix: pytest test file

--- src/scripts/python/python-tests.sh
+++ src/scripts/python/python-tests.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
-
 # Pytest
 if [[ "${EXTRA_PARAMS}" == "" ]]; then
+  poetry run pytest -vv --junitxml=tests/junit.xml
+else
   # shellcheck disable=SC2086
   poetry run pytest -vv --junitxml=tests/junit.xml ${EXTRA_PARAMS}
-else
-  poetry run pytest -vv --junitxml=tests/junit.xml
 fi
 # Coverage
 poetry run coverage html
