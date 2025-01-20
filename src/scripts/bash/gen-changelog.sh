56bc46b8bd16cfd1924eaa5f73de62ab65c9bd86
Julien Brayere <julien.brayere@obitrain.com>
2025-01-20 16:19:23 +0100

    chore: wip

--- src/scripts/bash/gen-changelog.sh
+++ src/scripts/bash/gen-changelog.sh
@@ -4,7 +4,7 @@ set -xeuo pipefail
 
 _TAG_PATTERN=${TAG_PATTERN:-"^v\d+\.\d+\.\d+$"}
 
-if ! uvx git-cliff -o CHANGELOG.md --latest --tag-pattern "$_TAG_PATTERN"; then
+if ! uvx git-cliff -o CHANGELOG.md --unreleased --tag-pattern "$_TAG_PATTERN"; then
     echo "Error: Failed to generate CHANGELOG" >&2
     exit 1
 fi
