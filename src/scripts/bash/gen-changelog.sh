5b17ea67984bfdab8e84772fd02d5f4d617e99dd
Julien Brayere <julien.brayere@obitrain.com>
2025-01-21 11:01:25 +0100

    fix: changelog tag

--- src/scripts/bash/gen-changelog.sh
+++ src/scripts/bash/gen-changelog.sh
@@ -3,10 +3,16 @@
 set -xeuo pipefail
 
 _CUSTOM_ARGS="${CUSTOM_ARGS:-}"
-
 _TAG_PATTERN=${TAG_PATTERN:-"^v\d+\.\d+\.\d+$"}
 
-if ! uvx git-cliff -o CHANGELOG.md --unreleased --tag-pattern "$_TAG_PATTERN" "$_CUSTOM_ARGS"; then
+if [ -f .tag ]; then
+    TAG=$(cat .tag)
+fi
+
+[[ -z "${TAG:-}" ]] && echo "TAG is not set" && exit 1
+
+
+if ! uvx git-cliff -o CHANGELOG.md --unreleased --tag-pattern "$_TAG_PATTERN" --tag "$TAG"  "$_CUSTOM_ARGS"; then
     echo "Error: Failed to generate CHANGELOG" >&2
     exit 1
 fi
