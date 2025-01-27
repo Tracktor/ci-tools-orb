4396a917a72e7cd0e00a97d470c905036cc86a24
Julien Brayere <julien.brayere@obitrain.com>
2025-01-27 14:16:17 +0100

    fix: push release

--- src/scripts/bash/push-release.sh
+++ src/scripts/bash/push-release.sh
@@ -7,7 +7,7 @@ if [ -f .tag ]; then
 fi
 
 [[ -z "${TAG:-}" ]] && echo "TAG is not set" && exit 1
-[[ -z "${DEFAULT_BRANCH:-}" ]] && echo "DEFAULT_BRANCH is not set" && exit 1
+[[ -z "${STABLE_BRANCH:-}" ]] && echo "STABLE_BRANCH is not set" && exit 1
 _BRANCH="${BRANCH:-$(git branch --show-current)}"
 
 git tag "$TAG"
@@ -17,10 +17,12 @@ if ! git push origin "$_BRANCH" --tags; then
     exit 1
 fi
 
-if [ "$DEFAULT_BRANCH" = "$BRANCH" ]; then
+if [ "$STABLE_BRANCH" = "$_BRANCH" ]; then
     echo "Creating Github release $TAG"
     if ! gh release create -F CHANGELOG.md "$TAG"; then
         echo "Error: Failed to create Github release" >&2
         exit 1
     fi
+else
+  echo "Skipping Github release for branch $_BRANCH (stable branch: $STABLE_BRANCH)"
 fi
